b7f671164a2542ba15b2b3e91043ba7dfe9f40d0
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-09-04 00:32:32 -0700

    Updated sign in page

--- Frontend/app/(signin)/index.tsx
+++ Frontend/app/(signin)/index.tsx
@@ -1,10 +1,30 @@
-import { View, ImageBackground } from "react-native"
+import { View, ImageBackground, TextInput, Text } from "react-native"
 import { StyleSheet } from "react-native"
 
 export default function SignIn() {
     return (
         <View style={{flex: 1, backgroundColor: 'white'}}>
-            <ImageBackground source={require('./signin_bg.png')} resizeMode="repeat" tintColor="#448da5" style={styles.image_bg}></ImageBackground>
+            <ImageBackground source={require('./signin_bg.png')} resizeMode="repeat" tintColor="#448da5" style={styles.image_bg}>
+                <View style={styles.input_field}>
+                    <Text style={styles.input_heading}>Username</Text>
+                    <View style={styles.div_input}>
+                    <TextInput placeholder="Username" style={styles.input} /></View></View>
+                
+                <View style={styles.input_field}>
+                    <Text style={styles.input_heading}>Email</Text>
+                    <View style={styles.div_input}>
+                    <TextInput placeholder="Email" style={styles.input} /></View></View>
+
+                <View style={styles.input_field}>
+                    <Text style={styles.input_heading}>Password</Text>
+                    <View style={styles.div_input}>
+                    <TextInput placeholder="Password" style={styles.input} /></View></View>
+
+                <View style={styles.input_field}>
+                    <Text style={styles.input_heading}>Confirm Password</Text>
+                    <View style={styles.div_input}>
+                    <TextInput placeholder="Confirm Password" style={styles.input} /></View></View>
+            </ImageBackground>
         </View>
     )
 }
@@ -16,7 +36,23 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 40,
         borderTopRightRadius: 40,
         backgroundColor: '#a6d2e0',
-        overflow: "hidden"
+        overflow: "hidden",
+        alignItems: 'center',
     },
+    input_field: {
+        marginBottom: 50,
+    },
+    input_heading: {
+        fontSize: 15,
+    },
+    div_input: {
+        width: 300,
+        height: 30,
+        backgroundColor: 'purple',
+    },
+    input: {
+        flex: 1,
+        backgroundColor: 'white',
+    }
 });
 
