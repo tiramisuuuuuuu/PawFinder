d94d857b89dd5dcb40d16c1db7cff9b2087a5243
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-09-05 21:40:41 -0700

    Added splash screen

--- Frontend/app/(signin)/index.tsx
+++ Frontend/app/(signin)/index.tsx
@@ -1,29 +1,39 @@
 import { View, ImageBackground, TextInput, Text } from "react-native"
 import { StyleSheet } from "react-native"
+import Octicons from '@expo/vector-icons/Octicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 
 export default function SignIn() {
+
     return (
         <View style={{flex: 1, backgroundColor: 'white'}}>
             <ImageBackground source={require('./signin_bg.png')} resizeMode="repeat" tintColor="#448da5" style={styles.image_bg}>
                 <View style={styles.input_field}>
                     <Text style={styles.input_heading}>Username</Text>
                     <View style={styles.div_input}>
-                    <TextInput placeholder="Username" style={styles.input} /></View></View>
+                        <Octicons name="person" style={styles.icon} />
+                        <TextInput placeholder="Username" style={styles.input} /></View></View>
                 
                 <View style={styles.input_field}>
                     <Text style={styles.input_heading}>Email</Text>
                     <View style={styles.div_input}>
-                    <TextInput placeholder="Email" style={styles.input} /></View></View>
+                        <Octicons name="mail" style={styles.icon} />
+                        <TextInput placeholder="your_email_id@mail.com" style={styles.input} /></View></View>
 
                 <View style={styles.input_field}>
                     <Text style={styles.input_heading}>Password</Text>
                     <View style={styles.div_input}>
-                    <TextInput placeholder="Password" style={styles.input} /></View></View>
+                        <Octicons name="lock" style={styles.icon} />
+                        <TextInput placeholder="Password" style={styles.input} /></View></View>
 
                 <View style={styles.input_field}>
                     <Text style={styles.input_heading}>Confirm Password</Text>
                     <View style={styles.div_input}>
-                    <TextInput placeholder="Confirm Password" style={styles.input} /></View></View>
+                        <Octicons name="lock" style={styles.icon} />
+                        <TextInput placeholder="Confirm Password" style={styles.input} /></View></View>
+                
+                <MaterialCommunityIcons name="paw-outline" size={24} color="black" />
             </ImageBackground>
         </View>
     )
@@ -40,19 +50,35 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     input_field: {
-        marginBottom: 50,
+        marginBottom: 15,
     },
     input_heading: {
-        fontSize: 15,
+        fontSize: 14,
+        marginBottom: 3,
+        fontWeight: 'bold',
     },
     div_input: {
-        width: 300,
-        height: 30,
-        backgroundColor: 'purple',
+        width: 310,
+        height: 35,
+        backgroundColor: 'white',
+        borderColor: '#448da5',
+        borderWidth: 1,
+        borderRadius: 7,
+        overflow: 'hidden',
+        padding: 5,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    icon: {
+        fontSize: 17,
+        color: 'black',
+        paddingRight: 10,
     },
     input: {
         flex: 1,
         backgroundColor: 'white',
+        fontSize: 13,
+        color: 'grey',
     }
 });
 
--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,16 +1,48 @@
 import { Stack } from "expo-router"
 import { View, StyleSheet , Image} from "react-native"
+import * as SplashScreen from 'expo-splash-screen';
+import { useState, useEffect, useCallback, useContext } from "react";
+import { PageLoadContext } from './Context';
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+    const [loaded, setLoaded] = useState(false);
+
+
+    useEffect(() => {
+        async function test() {
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            setLoaded(true);
+        }
+        test();
+
+        }, []);
+
+    const loaded_callback = useCallback(async () => {
+        if (loaded == false) {
+            SplashScreen.preventAutoHideAsync();
+            }
+        else {
+            await SplashScreen.hideAsync();
+            }
+        }, [loaded])
+
+    if (loaded == false) {
+        return null
+    }
+
     return (
         <View style={{flex: 1}}>
             <View style={styles.header}>
                 <Image source={require('./pawfinder_header.png')} style={{flex: 1, resizeMode: 'contain'}}></Image>
             </View>
-            <Stack screenOptions={{headerShown: false}}>
-                <Stack.Screen name="(signin)" />
-                <Stack.Screen name="dashboard" />
-            </Stack>
+            <PageLoadContext.Provider value={{loaded, setLoaded}}>
+                <Stack screenOptions={{headerShown: false}}>
+                    <Stack.Screen name="(signin)" />
+                    <Stack.Screen name="dashboard" />
+                </Stack>
+            </PageLoadContext.Provider>
         </View>
     )
 }
