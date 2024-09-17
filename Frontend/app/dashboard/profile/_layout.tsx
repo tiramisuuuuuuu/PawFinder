541fc20897962bf8006022422976305954ec6af8
Grace Chen <75271809+gracexichen@users.noreply.github.com>
2024-09-16 21:47:14 -0700

    add profile page design

--- Frontend/app/dashboard/profile/_layout.tsx
+++ Frontend/app/dashboard/profile/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
 
-export default function HomeLayout() {
-    return (
-        <Stack screenOptions={{headerShown: false}}>
-            <Stack.Screen name="index" />
-        </Stack>
-    )
+export default function ProfileLayout() {
+	return (
+		<Stack screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="index" />
+			<Stack.Screen name="account" />
+		</Stack>
+	);
 }
