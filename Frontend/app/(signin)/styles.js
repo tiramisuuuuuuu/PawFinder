4b400d3437fd09aa8bcbff625ee05b617fdae8b0
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-09-19 14:31:16 -0700

    Loaded pet profiles in homepage

--- Frontend/app/(signin)/styles.js
+++ Frontend/app/(signin)/styles.js
@@ -3,8 +3,6 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        borderTopLeftRadius: 40,
-        borderTopRightRadius: 40,
         overflow: "hidden",
         alignItems: 'center'
     },
