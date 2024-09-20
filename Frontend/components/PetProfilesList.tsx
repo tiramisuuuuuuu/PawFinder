84b597abd9135cda3ad0eaa11f68aa9a16431e37
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-09-20 02:44:35 -0700

    Completed the /(home)/view/[id] page

--- Frontend/components/PetProfilesList.tsx
+++ Frontend/components/PetProfilesList.tsx
@@ -2,44 +2,52 @@ import { Pressable, View, Text, StyleSheet, Image } from "react-native";
 import { Link } from "expo-router";
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useEffect, useState } from "react";
+import Constants from 'expo-constants';
 
-//  * @param userToken - token of user that's adding the profile
-//  * @param petName - name of pet
-//  * @param petSpecies - species of pet
-//  * @param lastSeen - location the pet was last seen
-//  * @param petDescription - description of the pet
-//  * @param assignedTasks - tasks assigned
-const profiles = [
-    {
-        petName: "Doug",
-        petSpecies: "dog",
-        petBreed: "Golden Retriever",
-        posted: "09-01-24",
-        lastSeen: "Safeway",
-        petDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        assignedTasks: "please hold",
-        photoUrl: "https://furballfiesta.com/wp-content/uploads/2023/10/DogsHavaneseHavanese-Puppy-Watercolor-Clipart-2.jpg",
-        id: "123"
-    },
-    {
-        petName: "Bob",
-        petSpecies: "cat",
-        petBreed: "Siamese",
-        posted: "yesterday",
-        lastSeen: "Safeway",
-        petDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-        assignedTasks: "please hold",
-        photoUrl: "https://cdn.shopify.com/s/files/1/0416/8083/0620/files/01132022_soc_pinterestboardcoverupdate_CH_1200x1200_4ea79688-a3c1-4e16-beed-e57305950964_480x480.png?v=1651008658",
-        id: "457"
-    },
-]
+
+async function getPetProfiles(lat: Number, lng: Number) {
+    try {
+        const targetUrl = `http://${Constants.expoConfig?.extra?.backendURL}/getNearbyPetProfiles/`;
+        const response = await fetch(targetUrl, {
+            method: "post",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                latitude: lat,
+                longitude: lng,
+            }),
+        })
+        const arr = await response.json();
+        return arr;
+    } catch {
+        console.log("network issue.");
+        return null;
+    }
+}
+
 export default function PetProfilesList({latLng}) {
+    const [profiles, setPetProfiles] = useState([]);
+
+    useEffect(()=>{
+        async function initialize() {
+            const [lat, lng] = latLng.split(', ')
+            const resultsArr = await getPetProfiles(Number(lat), Number(lng));
+            if (resultsArr != null) {
+                setPetProfiles(resultsArr);
+            }
+        }
+
+        initialize();
+    }, [latLng])
 
     return (
         <View style={{width: '100%', marginTop: 30}}>
             <Text style={{width: '100%', fontFamily: 'LilitaOne-Regular', fontSize: 27, marginBottom: 20}}>{`(${profiles.length}) Missing Pets Near You`}</Text>
             {profiles.map((profile)=>{ return (
-                <View style={styles.container} key={profile.id}>
+                <View style={styles.container} key={profile._id}>
                     <Image source={{ uri: profile.photoUrl }} resizeMode="contain" style={{width: '100%', height: '42%'}} />
                     <View style={{flex:1, width: '100%', borderTopWidth: 1, borderTopColor: 'grey', backgroundColor: '#e2e1e5', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', padding: 20}}>
                         <View style={{flexDirection: 'row', width: '100%', marginBottom: 20, alignItems: 'center', justifyContent: 'flex-start'}}>
@@ -50,7 +58,7 @@ export default function PetProfilesList({latLng}) {
                         <View style={{flexDirection: 'row', width: '100%', marginBottom: 10, alignItems: 'center', justifyContent: 'flex-start'}}>
                             <Text style={{fontFamily: 'Poppins-Regular', fontSize: 12, marginRight: 5}}>
                                 <Text style={{color: 'grey'}}>Posted: </Text>
-                                <Text>{profile.posted}</Text>
+                                <Text>{profile.postedDate}</Text>
                             </Text>
                             <FontAwesome name="clock-o" size={15} color="grey" style={{marginRight: 15}} />
                             <Text style={{fontFamily: 'Poppins-Regular', fontSize: 12, marginRight: 5}}>
@@ -61,7 +69,7 @@ export default function PetProfilesList({latLng}) {
                         </View>
                         <Text ellipsizeMode='tail' numberOfLines={2} style={{width: '100%', textAlign: 'left', fontFamily: 'Poppins-Regular', fontSize: 15, marginBottom: 10}}>{profile. petDescription}</Text>
                         <Pressable style={{marginTop: 5, paddingTop: 5, paddingBottom: 5, paddingRight: 10, paddingLeft: 10, backgroundColor: 'teal', borderRadius: 10}}>
-                            <Link href={`./view//${profile.id}`} onPress={()=>{console.log("start search pressed")}}>
+                            <Link href={`./view//${profile._id}`} onPress={()=>{console.log("start search pressed")}}>
                                 <Text style={{fontFamily: 'Poppins-Regular', fontSize: 15, color: 'white'}}>Start Search</Text>
                             </Link>
                         </Pressable>
